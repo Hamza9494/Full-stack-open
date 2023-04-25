63c2284e53dc9de3731ab23ea1830fdb20c21cc6
Hamza9494 <baitichhamza@gmail.com>
2023-04-25 15:03:01 +0100

    fix modules bug

--- part3/Course_information/src/App.js
+++ part3/Course_information/src/App.js
@@ -1,49 +1,4 @@
-const Part = ({ note }) => {
-  return (
-    <p>
-      {' '}
-      {note.name} {note.exercises}{' '}
-    </p>
-  )
-}
-
-const Content = ({ course }) => {
-  const parts = course.parts
-  return (
-    <div className="content">
-      {parts.map((note) => (
-        <Part key={note.id} note={note} />
-      ))}
-    </div>
-  )
-}
-
-const Header = ({ course }) => {
-  return <h1>{course.name} </h1>
-}
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((acc, cur) => acc + cur.exercises, 0)
-  console.log(sum)
-  return <p> total of {sum} exercises </p>
-}
-
-const Courses = ({ courses }) => {
-  return (
-    <div className="course">
-      {courses.map((course) => {
-        return (
-          <div className="course" key={course.id}>
-            <Header course={course} />
-            <Content course={course} />
-            <Total parts={course.parts} />
-          </div>
-        )
-      })}
-    </div>
-  )
-}
-
+import Coursess from './Coursess'
 const App = () => {
   const courses = [
     {
@@ -89,7 +44,7 @@ const App = () => {
       ],
     },
   ]
-  return <Courses courses={courses} />
+  return <Coursess courses={courses} />
 }
 
 export default App
